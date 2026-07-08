a4b9fced919f9ce7ccad91ed94ec5fbbbec5051c
omaralazawy871-spec <omaralazawy871@gmail.com>
2026-07-08 03:20:42 +0300

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.example.iptv"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "com.example.iptv"
+        minSdk = 21
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+}

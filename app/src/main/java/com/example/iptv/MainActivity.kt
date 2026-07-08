79a3f00d5bd809447da232f4d9765e8a7a620f7a
omaralazawy871-spec <omaralazawy871@gmail.com>
2026-07-08 03:59:05 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/iptv/MainActivity.kt
+++ app/src/main/java/com/example/iptv/MainActivity.kt
@@ -1,27 +1,24 @@
 package com.example.iptv
 
+import android.app.Activity
 import android.os.Bundle
 import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         
         val webView = WebView(this)
         
-        // تفعيل إعدادات الجافا سكريبت والميديا عشان يشتغل الفيديو
         webView.settings.javaScriptEnabled = true
-        webView.settings.mediaPlaybackRequiresUserGesture = false // السماح بالتشغيل التلقائي
+        webView.settings.mediaPlaybackRequiresUserGesture = false 
         webView.webViewClient = WebViewClient()
-        webView.webChromeClient = WebChromeClient() // ضروري لدعم مشغل الفيديو
+        webView.webChromeClient = WebChromeClient() 
         
-        // رابط القناة مالتك
         val videoUrl = "https://m3uextractor.indexiptv212.workers.dev/download/0241cbac-61a4-4b8e-9973-1c289d2232fc/Live_AR%20_%20ALWAN%20VIP.m3u8"
         
-        // تصميم صفحة ويب صغيرة باللون الأسود تحتوي على مشغل فيديو يدعم البث
         val htmlData = """
             <html>
             <body style="margin:0;padding:0;background-color:black;display:flex;justify-content:center;align-items:center;">
@@ -33,7 +30,6 @@ class MainActivity : AppCompatActivity() {
             </html>
         """.trimIndent()
         
-        // تحميل الصفحة المصممة بدل تحميل الرابط مباشرة
         webView.loadData(htmlData, "text/html", "UTF-8")
         
         setContentView(webView)

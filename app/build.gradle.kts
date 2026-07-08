269ca90ee68e27e425687dadc9cd789e7a2f2dee
omaralazawy871-spec <omaralazawy871@gmail.com>
2026-07-08 04:52:29 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,26 +4,14 @@ plugins {
 }
 
 android {
-    namespace = "com.example.iptv"
+    namespace = "com.example.mytv2"
     compileSdk = 33
 
     defaultConfig {
-        applicationId = "com.example.iptv"
+        applicationId = "com.example.mytv2"
         minSdk = 21
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
     }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-}
-
-dependencies {
-    implementation("androidx.core:core-ktx:1.9.0")
 }
